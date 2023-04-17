1782d3f875ee85fda4a24764daac224076c5c70d
Yato <yatoakii@gmail.com>
2023-04-17 10:41:24 +0630

    Added: Projects with icons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,26 +11,29 @@ const Projects = () => {
             pic:Project1,
             title:"Selfie Generator",
             emoji:"📸",
-            text:"Upload your photo and swap the background with a background art to make it pretty."
+            text:"Upload your photo and swap the background with a background art to make it pretty.",
+            tech:["html","css","react","figma"]
         },
         {
             pic:Project2,
             title:"5 Minute Chat",
             emoji:"⏳",
-            text:"Share your code and create a chat room that only exist for 5 minutes. Try to get to know someone within time limit."
+            text:"Share your code and create a chat room that only exist for 5 minutes. Try to get to know someone within time limit.",
+            tech:["html","css","react","firebase","tailwind"]
         },
         {
             pic:Project3,
             title:"Yours Resolution",
             emoji:"📝",
-            text:"Answer questions and generate a code to share your new year resolution with loved ones. (Hackathon winning project)"
+            text:"Answer questions and generate a code to share your new year resolution with loved ones. (Hackathon winning project)",
+            tech:["html","css","react","firebase","tailwind"]
         }
     ]  
 
     return(
         <div>
             {projectData.map((data,key) => {
-                return <ProjectUI pic={data.pic} title={data.title} text={data.text} emoji={data.emoji} direction={key%2 == 0 ? "right" : "left"}/>
+                return <ProjectUI pic={data.pic} title={data.title} text={data.text} emoji={data.emoji} direction={key%2 === 0 ? "right" : "left"} tech={data.tech}/>
             })}
             
         </div>
--- src/Components/toolComponents/ProjectUI.js
+++ src/Components/toolComponents/ProjectUI.js
@@ -4,7 +4,6 @@ import css from "../../assets/🦆 icon _file type css_.png"
 import firebase from "../../assets/🦆 icon _file type firebase_.png"
 import html from "../../assets/🦆 icon _file type html_.png"
 import reactjs from "../../assets/🦆 icon _file type reactjs_.png"
-import js from "../../assets/Group 15.png"
 import figma from "../../assets/Figma (icon — Colour).png"
 import tailwind from "../../assets/🦆 icon _file type tailwind_.png"
 import ImageUI from "./ImageUI";
@@ -22,6 +21,9 @@ const ProjectUI = (props) => {
             <div className="textArea" style={rightText}>
                 <h1 >{props.title}<span>{props.emoji}</span></h1>
                 <p>{props.text}</p>
+                <div className="icons">
+                    {textIconGenerator()}
+                </div>
                 <button>Check on GitHub</button>
             </div>
         )
@@ -37,9 +39,47 @@ const ProjectUI = (props) => {
             <div className="textArea" style={leftText}>
                 <h1 >{props.title}<span>{props.emoji}</span></h1>
                 <p>{props.text}</p>
+                <div className="icons">
+                    {textIconGenerator()}
+                </div>
                 <button>Check on GitHub</button>
             </div>
         )
+    }
+
+    const textIconGenerator = () => {
+        console.log(props.tech)
+        return(
+            props.tech.map((name) => {
+                if (name === "html"){
+                    return(<img src={html} alt="" />)
+                }
+
+                if (name === "css"){
+                    return(<img src={css} alt="" />)
+                }
+
+                if (name === "reactjs"){
+                    return(<img src={reactjs} alt="" />)
+                }
+
+                if (name === "firebase"){
+                    return(<img src={firebase} alt="" />)
+                }
+
+                if (name === "tailwind"){
+                    return(<img src={tailwind} alt="" />)
+                }
+
+                if (name === "figma"){
+                    return(<img src={figma} alt="" />)
+                }
+
+                return null
+            }
+            )
+        )
+        
     }
 
     return(
